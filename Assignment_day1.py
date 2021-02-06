5fd5172ceefbc61474ec2f29ad0c385cf506ce4b
sidharth48-hub <75977972+sidharth48-hub@users.noreply.github.com>
2021-02-07 00:25:56 +0530

    Assignment 1 submitted

---
+++ Assignment_day1.py
@@ -0,0 +1,14 @@
+x=["shubham",22,"cse","python",55.5]
+x.insert(1,"sarda")
+x.remove("cse")
+x.sort()
+x.rev()
+x.append(70)
+
+emp={"name":"shubham","age":22,"sal":100000,"job":"developer"}
+emp.get("name")
+emp.pop("sal")
+emp.values()
+emp.update({"name":"shubham sarda"})
+emp.clear()
+
